ef98a984edbb4f6ca191259036cdee241d9c56cf
Johann Blignaut <blignaut24@gmail.com>
2025-01-12 14:31:35 +0000

    fix: set pagination limit to 6 posts per page in postlist view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
+# Django imports for rendering views and handling HTTP responses
 from django.shortcuts import render, HttpResponse
+from django.views import generic
+
+# Import models from current directory
+from .models import Blogpost, Comment, MediaCategory
 
 
-# Create your views here.
+# Basic view function to test server connectivity
 def index(request):
-    return HttpResponse("Hello,World!")
+    return HttpResponse("Hello, World!")
+
+
+# List View for Blog Posts
+class PostList(generic.ListView):
+    """
+    Shows a list of blog posts with 6 posts per page, using the Blogpost model to get only published posts.
+    """
+    model = Blogpost
+    queryset = Blogpost.objects.filter(status=1).order_by('created_on')
+    template_name = 'index.html'
+    paginate_by = 6
