f340504118bef73d1263af3d028d8bac2de90a94
Johann Blignaut <blignaut24@gmail.com>
2025-01-14 12:29:45 +0000

    style: add comments to blogpost pagination view code

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # Django imports for rendering views and handling HTTP responses
-from django.shortcuts import render, HttpResponse
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Blogpost, Comment, MediaCategory
 
 # Import models from current directory
 from .models import Blogpost, Comment, MediaCategory
@@ -22,3 +23,30 @@ class BlogpostPostList(generic.ListView):
     context_object_name = 'blogposts'
     template_name = "index.html"
     paginate_by = 6
+
+
+# Blog post detail view
+class BlogPostDetail(View):
+    """
+    Displays blog posts with comments and user like status.
+    """
+    def get(self, request, slug, *args, **kwargs):
+        # Get published post or 404
+        queryset = Blogpost.objects.filter(status=1)
+        blogpost = get_object_or_404(queryset, slug=slug)
+        # Get approved comments
+        comments = blogpost.comments.filter(approved=True).order_by("-created_on")
+        # Check if user has liked post
+        liked = False
+        if blogpost.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        # Render template with context
+        return render(
+            request,
+            "blogpost_detail.html",
+            {
+                "blogpost": blogpost,
+                "comments": comments,
+                "liked": liked
+            },
+        )
