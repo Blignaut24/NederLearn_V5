26865a82175bde93a286bacaab73baad0fa6c852
Johann Blignaut <blignaut24@gmail.com>
2025-01-21 15:09:22 +0000

    style: refactor comments section layout and structure in blogpost_detail template

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,27 @@
 from .models import Comment
 from django import forms
+
+
 class CommentForm(forms.ModelForm):
+    """
+        This is a form that handles user comments.
+
+    It includes:
+    • A space for writing comments
+    • Rules for how comments should look
+    • Settings for the comment box
+    """
+
     class Meta:
         model = Comment
-        fields = ('body',)
+        fields = ("body",)
+        widgets = {
+            "body": forms.Textarea(
+                attrs={
+                    "rows": 2,  # Height of text area
+                    "cols": 50,  # Width of text area
+                    "placeholder": "Drop your thoughts here like they're hot! 🔥",  # Default text
+                    "max_length": "1500",  # Maximum characters
+                }
+            ),
+        }
