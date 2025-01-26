f3946627796a823295bcc3a7767fde9504669526
Johann Blignaut <blignaut24@gmail.com>
2025-01-26 08:21:35 +0000

    fix: correct account_delete pattern in urls.py

--- nederlearn/urls.py
+++ nederlearn/urls.py
@@ -21,7 +21,6 @@ URL Pattern Types:
    from django.urls import include
    path('blog/', include('blog.urls'))
 """
-
 # Django Core Imports
 from django.contrib import admin
 from django.urls import path, include
@@ -33,22 +32,18 @@ from blog.views import ProfileDeleteView, custom_403_error
 
 # Main URL Patterns
 urlpatterns = [
-    # Admin Interface
     path("admin/", admin.site.urls),
-    # Blog URLs (Root URL Pattern)
     path("", include("blog.urls"), name="blog-urls"),
-    # Rich Text Editor
     path("summernote/", include("django_summernote.urls")),
-    # Authentication URLs
     path("accounts/", include("allauth.urls")),
-    # Profile Management
-    path("account/delete//", ProfileDeleteView.as_view(), name="account_delete"),
+    path(
+        "account/delete/<int:pk>/", ProfileDeleteView.as_view(), name="account_delete"
+    ),
 ]
 
 # Development Media File Serving
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-# Custom Error Handlers
-handler403 = custom_403_error  # Forbidden Error Handler_error
+# Forbidden Error Handler_error
+handler403 = custom_403_error
