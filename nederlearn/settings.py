a3ad9a2792544af710fb5b08e37784b02f716a9f
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2025-06-18 22:02:35 +0200

    fix: resolve django-crispy-forms template error

--- nederlearn/settings.py
+++ nederlearn/settings.py
@@ -40,13 +40,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # Do NOT use in production without modification
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 
 # Host Configuration
 # -----------------
 ALLOWED_HOSTS = [
     # Development Environments
     "localhost",
+    "127.0.0.1",
     # Production Environments
     "nederlearn-v4.herokuapp.com",  # Legacy production server
     "nederlearn-v5-c628536a9899.herokuapp.com",  # Current production server
@@ -83,6 +84,7 @@ INSTALLED_APPS = [
     ###########################
     "django_summernote",  # Rich text editor
     "crispy_forms",  # Form styling and layout
+    "crispy_bootstrap4",  # Bootstrap 4 template pack for crispy forms
     ###########################
     # 5. Custom Applications  #
     ###########################
@@ -110,7 +112,7 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 # Form Styling
 # -----------
 # Bootstrap 4 styling for Crispy Forms
-CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"  
 
 # =================================================================
 # MESSAGE TAGS CONFIGURATION
